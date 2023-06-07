fb6267a8c791e35be1174a77e050cd7056654b71
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-06-06 20:25:06 -0500

    Added second submenu & allowed for IMDB or google search

--- background.js
+++ background.js
@@ -8,14 +8,34 @@ chrome.runtime.onInstalled.addListener((details) => {
   });
   // Child Context Menu
   chrome.contextMenus.create({
-    title: "Child Menu",
+    title: "Google Search",
     id: "contextMenu2",
     parentId: "contextMenu1",
     // Context Menu Show up on right click of page or menu selection
-    contexts: ["page", "selection"],
+    contexts: ["selection"],
   });
+  // Child menu 2
+  chrome.contextMenus.create({
+    title: "IMDB Search",
+    id: "contextMenu3",
+    parentId: "contextMenu1",
+    // Context Menu Show up on right click of page or menu selection
+    contexts: ["selection"],
+  });
+
   chrome.contextMenus.onClicked.addListener((event) => {
-    console.log(event);
+    // Check if menu 2 or menu 3 has been clicked
+    if (event.menuItemId === "contextMenu2") {
+      chrome.search.query({
+        // On click create new tab and send the selected text
+        disposition: "NEW_TAB",
+        text: `${event.selectionText}`,
+      });
+    } else if (event.menuItemId === "contextMenu3") {
+      chrome.tabs.create({
+        url: `https://www.imdb.com/find/?q=${event.selectionText}&ref_=nv_sr_sm`,
+      });
+    }
   });
 });
 
