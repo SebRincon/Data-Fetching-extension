342000801f5ad72169b0464b55f5e4e21461b5c5
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-06-06 20:01:59 -0500

    Added on click listener and child menus for context menu

--- background.js
+++ background.js
@@ -6,6 +6,17 @@ chrome.runtime.onInstalled.addListener((details) => {
     // Context Menu Show up on right click of page or menu selection
     contexts: ["page", "selection"],
   });
+  // Child Context Menu
+  chrome.contextMenus.create({
+    title: "Child Menu",
+    id: "contextMenu2",
+    parentId: "contextMenu1",
+    // Context Menu Show up on right click of page or menu selection
+    contexts: ["page", "selection"],
+  });
+  chrome.contextMenus.onClicked.addListener((event) => {
+    console.log(event);
+  });
 });
 
 console.log("Background is Running");
